baa4eb5562ba7a4c654da2fb8ace5e075a7451ba
Double_J <double04j@gmail.com>
2023-07-07 12:21:50 +0900

    모델객체 반환방법 변경

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -0,0 +1,65 @@
+package hello.servlet.web.frontcontroller.v4;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v3.Controllerv3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
+import org.springframework.ui.Model;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
+public class FrontControllerServletV4 extends HttpServlet {
+
+    private Map<String, ControllerV4> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV4() {
+        controllerMap.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+        controllerMap.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+        controllerMap.put("/front-controller/v4/members", new MemberListControllerV4());
+
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV4.service");
+
+        // /front-controller/v2/*
+        String requestURI = req.getRequestURI();
+        ControllerV4 controller = controllerMap.get(requestURI);
+        if (controller == null) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);//404 페이지가 없는 경우
+            return;
+        }
+
+        Map<String, String> paramMap = createParamMap(req);
+        Map<String, Object> model = new HashMap<>();//추가된 부분
+        String viewName = controller.process(paramMap, model);
+        
+        MyView view = viewResolver(viewName);//논리이름을 물리이름으로 변환
+        view.render(model,req,resp);
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest req) {
+        Map<String,String> paramMap = new HashMap<>();
+        req.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, req.getParameter(paramName)));
+        return paramMap;
+    }
+}
